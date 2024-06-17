21d122d4040af1c7caaf712a25eb580aa5ff335a
alonWohl <alonw1010@gmail.com>
2024-06-17 11:37:16 +0300

    Add dynamic rendering of books

--- js/book.controller.js
+++ js/book.controller.js
@@ -1,9 +1,25 @@
 'use strict'
 
-function onInit(){
-    render()
+function onInit() {
+  render()
 }
 
-function render(){
+function render() {
+  const books = getBooks()
+  const tableBody = document.querySelector('tbody')
 
+  tableBody.innerHTML = books
+    .map(
+      (book) => `
+        <tr>
+            <td>${book.title}</td>
+            <td>${book.price}</td>
+            <td>
+                <button class"read">Read</button>
+                <button class"update">Update</button>
+                <button class"delete">Delete</button>
+            </td>
+        </tr>
+    `
+    ) .join('')
 }
