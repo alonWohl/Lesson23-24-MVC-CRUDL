eb9af644ddf914917def176825e5c122314efadf
alonWohl <alonw1010@gmail.com>
2024-06-17 11:30:03 +0300

    Init repo

---
+++ js/services/book.service.js
@@ -0,0 +1,24 @@
+'use strict'
+
+
+const gBooks = [
+    {
+        id: 1,
+        title: 'The Adventures of Lori Ipsi',
+        price: 120
+    },
+    {
+        id: 2,
+        title: 'World Atlas',
+        price: 300
+    },
+    {
+        id: 3,
+        title: 'Zorba the Greek',
+        price: 87
+    }
+];
+
+function getBooks() {
+    return gBooks;
+}
